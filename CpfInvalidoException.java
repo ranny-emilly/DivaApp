bbb0d2d8e74d1f3e1409030791c9463b6ee5eee0
Jolie Pavan <jolie@discente.ufg.br>
2026-06-26 12:35:07 -0300

    Update CpfInvalidoException.java

--- CpfInvalidoException.java
+++ CpfInvalidoException.java
@@ -1,18 +1,7 @@
 package com.lojamoda.exception;
 
-/**
- * Exceção lançada quando um CPF informado para um Cliente é inválido
- * (não respeita o formato esperado) ou já está cadastrado para outro cliente.
- *
- * @author Elisa Correia, Emilly Ranny, Jolie Pavan
- */
 public class CpfInvalidoException extends Exception {
 
-    /**
-     * Cria uma nova exceção de CPF inválido com a mensagem informada.
-     *
-     * @param mensagem descrição do motivo da invalidade do CPF
-     */
     public CpfInvalidoException(String mensagem) {
         super(mensagem);
     }
