709d58704045af7952252c29dd59b3d16c64d9cf
Emilly Ranny <ranny.emilly@discente.ufg.br>
2026-06-26 23:04:28 -0300

    Delete EntidadeNaoEncontradaException.java

--- EntidadeNaoEncontradaException.java
+++
@@ -1,8 +0,0 @@
-package com.lojamoda.exception;
-
-public class EntidadeNaoEncontradaException extends Exception {
-
-    public EntidadeNaoEncontradaException(String tipoEntidade, int id) {
-        super(String.format("%s com ID %d não foi encontrado(a).", tipoEntidade, id));
-    }
-}
