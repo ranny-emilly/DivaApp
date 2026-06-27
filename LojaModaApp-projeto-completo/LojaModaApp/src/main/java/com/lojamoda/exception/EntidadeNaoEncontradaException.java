f799136a9908255b6d4a14f7f07e0bd123dbf547
Jolie Pavan <jolie@discente.ufg.br>
2026-06-27 11:23:09 -0300

    Update EntidadeNaoEncontradaException.java

--- LojaModaApp-projeto-completo/LojaModaApp/src/main/java/com/lojamoda/exception/EntidadeNaoEncontradaException.java
+++ LojaModaApp-projeto-completo/LojaModaApp/src/main/java/com/lojamoda/exception/EntidadeNaoEncontradaException.java
@@ -1,21 +1,7 @@
 package com.lojamoda.exception;
 
-/**
- * Exceção lançada quando uma operação de consulta, alteração ou exclusão
- * é solicitada para um identificador (ID) que não existe na base de dados
- * em memória (Cliente, Produto ou Pedido).
- *
- * @author Elisa Correia, Emilly Ranny, Jolie Pavan
- */
 public class EntidadeNaoEncontradaException extends Exception {
 
-    /**
-     * Cria uma nova exceção informando que a entidade do tipo indicado
-     * com o ID informado não foi localizada.
-     *
-     * @param tipoEntidade nome do tipo de entidade (ex: "Cliente", "Produto", "Pedido")
-     * @param id           identificador que não foi encontrado
-     */
     public EntidadeNaoEncontradaException(String tipoEntidade, int id) {
         super(String.format("%s com ID %d não foi encontrado(a).", tipoEntidade, id));
     }
